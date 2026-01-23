c94a3826fa31163f719ac7731248b894d5a64e28
Omega Joctan <omegajoctan@gmail.com>
2026-01-23 20:27:24 +0300

    history manager testing

--- tests/hist_manager_test.py
+++ tests/hist_manager_test.py
@@ -1,12 +1,43 @@
-import MetaTrader5 as mt5
-from Trade.Trade import CTrade
-from datetime import datetime, timedelta
-import time
-import pytz
-from tester import Tester
-from Trade.Trade import CTrade
+from strategytester5.hist.manager import HistoryManager
+from strategytester5.validators.tester_configs import TesterConfigValidators
 
-if not mt5.initialize(): # Initialize MetaTrader5 instance
-    print(f"Failed to Initialize MetaTrader5. Error = {mt5.last_error()}")
-    mt5.shutdown()
-    quit()
+history_dir = "Test History"
+    
+if __name__ == "__main__":
+
+    tester_config: dict = {
+        "bot_name": "test EA",
+        "deposit": 1000,
+        "leverage": "1:100",
+        "timeframe": "H1",
+        "start_date": "01.01.2025 00:00",
+        "end_date": "31.12.2025 00:00",
+        "modelling": "1-minute-ohlc",
+        # "modelling": "new_bar",
+        "symbols": ["EURUSD", "GBPUSD", "USDCAD"]
+    }
+
+    tester_config = TesterConfigValidators.parse_tester_configs(tester_config)
+        
+    start_dt = tester_config["start_date"]
+    end_dt   = tester_config["end_date"]
+    symbols = tester_config["symbols"]
+    timeframe = tester_config["timeframe"]
+    modelling = tester_config["modelling"]
+
+
+    import MetaTrader5 as mt5
+    
+
+    if not mt5.initialize():
+        print("Failed to initialize MetaTrader5, Error = ", mt5.last_error())
+        quit()
+
+    hist_manager = HistoryManager(mt5_instance=mt5,
+                                start_dt=start_dt,
+                                end_dt=end_dt,
+                                symbols=symbols,
+                                timeframe=timeframe)
+
+    hist_manager.fetch_history(modelling=modelling)
+    hist_manager.synchronize_timeframes()
