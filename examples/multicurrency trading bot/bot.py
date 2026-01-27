c025aa498cf7756eb44b756daf3aadffb46915d3
Omega Joctan <omegajoctan@gmail.com>
2026-01-27 12:36:51 +0300

    multicurrency trading robot example

--- examples/multicurrency trading bot/bot.py
+++ examples/multicurrency trading bot/bot.py
@@ -1,63 +1,83 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 import MetaTrader5 as mt5
 from strategytester5.tester import StrategyTester
 from strategytester5.trade_classes.Trade import CTrade
 import json
 import os
-import sys
+import logging
 
 if not mt5.initialize(): # Initialize MetaTrader5 instance
     print(f"Failed to Initialize MetaTrader5. Error = {mt5.last_error()}")
     mt5.shutdown()
     quit()
-    
+
 # Get path to the folder where this script lives
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 try:
     with open(os.path.join(BASE_DIR, "tester.json"), 'r', encoding='utf-8') as file: # reading a JSON file
         # Deserialize the file data into a Python object
-        tester_configs = json.load(file)
+        configs_json = json.load(file)
 except Exception as e:
     raise RuntimeError(e)
 
-tester = StrategyTester(tester_config=tester_configs["tester"], mt5_instance=mt5) # very important
+tester_configs = configs_json["tester"]
+tester = StrategyTester(tester_config=tester_configs, mt5_instance=mt5, logging_level=logging.DEBUG) # very important
 
 # ---------------------- inputs ----------------------------
 
-symbol = "EURUSD"
 timeframe = "PERIOD_H1"
 magic_number = 10012026
 slippage = 100
 sl = 1000
 tp = 100
 
+symbols = tester_configs["symbols"]
+timeframes = [mt5.TIMEFRAME_M15, mt5.TIMEFRAME_H1, mt5.TIMEFRAME_H4, mt5.TIMEFRAME_D1]
+
 # ---------------------------------------------------------
 
-m_trade = CTrade(simulator=tester, magic_number=magic_number, filling_type_symbol=symbol, deviation_points=slippage)
-symbol_info = tester.symbol_info(symbol=symbol)
+m_trade_objects = {
+    symbol: CTrade(
+        simulator=tester,
+        magic_number=magic_number,
+        filling_type_symbol=symbol,
+        deviation_points=slippage
+    )
+    for symbol in symbols
+}
 
-def pos_exists(magic: int, type: int) -> bool:
+def pos_exists(magic: int, symbol: str, type: int) -> bool:
 
     for position in tester.positions_get():
-        if position.type == type and position.magic == magic:
+        if position.type == type and position.magic == magic and position.symbol == symbol:
             return True
-    
+
     return False
 
-def on_tick():
-    
+def on_tick_multicurrency(symbol: str):
+
+    m_trade = m_trade_objects[symbol]
     tick_info = tester.symbol_info_tick(symbol=symbol)
-    
+
+    if tick_info is None:  # if the process of obtaining ticks wasn't successful
+        return
+
     ask = tick_info.ask
     bid = tick_info.bid
-    
-    pts = symbol_info.point
-    
-    if not pos_exists(magic=magic_number, type=mt5.POSITION_TYPE_BUY): # If a position of such kind doesn't exist
-        m_trade.buy(volume=0.1, symbol=symbol, price=ask, sl=ask-sl*pts, tp=ask+tp*pts, comment="Tester buy") # we open a buy position
-    
-    if not pos_exists(magic=magic_number, type=mt5.POSITION_TYPE_SELL): # If a position of such kind doesn't exist
-        m_trade.sell(volume=0.1, symbol=symbol, price=bid, sl=bid+sl*pts, tp=bid-tp*pts, comment="Tester sell") # we open a sell position
-    
 
-tester.OnTick(ontick_func=on_tick) # very important!
+    symbol_info = tester.symbol_info(symbol)
+    pts = symbol_info.point
+    volume = symbol_info.volume_min
+
+    if not pos_exists(magic=magic_number, symbol=symbol, type=mt5.POSITION_TYPE_BUY):  # If a position of such kind doesn't exist
+        m_trade.buy(volume=volume, symbol=symbol, price=ask, sl=ask - sl * pts, tp=ask + tp * pts, comment="Tester buy")  # we open a buy position
+
+    if not pos_exists(magic=magic_number, symbol=symbol, type=mt5.POSITION_TYPE_SELL):  # If a position of such kind doesn't exist
+        m_trade.sell(volume=volume, symbol=symbol, price=bid, sl=bid + sl * pts, tp=bid - tp * pts, comment="Tester sell")  # we open a sell position
+
+tester.ParallelOnTick(ontick_func=on_tick_multicurrency) # very important!
